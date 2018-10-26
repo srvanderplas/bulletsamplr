80e42eeb950a40acd39c1159cfd5d654a5fa826f
srvander <srvanderplas@gmail.com>
2018-10-26 13:59:54 -0500

    Multicycle chunks, paper edits

--- R/get_cycle.R
+++ R/get_cycle.R
@@ -1,25 +1,19 @@
-## @useDynLib bulletsamplr
-## @importFrom Rcpp sourceCpp
-## NULL
-#
-# .onUnload <- function(libpath) {
-#   library.dynam.unload("bulletsamplr", libpath)
-# }
-
-#' Slice signature into cycles
+#' Slice signature into chunks consisting of N cycles
 #'
 #' @param x a numeric vector with signature information
+#' @param ncycle number of cycles per chunk
 #' @return a list of vectors with signature cycles
 #' @importFrom assertthat assert_that has_name
 #' @import dplyr
 #' @importFrom purrr pmap
 #' @importFrom bulletxtrctr sig_get_peaks
+#' @importFrom stats median
 #' @export
 #' @examples
 #' data("sig")
 #' crosscut_slice(sig) # Slice whole data frame
-#' crosscut_slice(sig$sig) # Slice signature only
-crosscut_slice <- function(x) {
+#' crosscut_slice(sig$sig, ncycle = 3) # Slice signature only, using 3 cycles/chunk
+crosscut_slice <- function(x, ncycle = 1) {
   .type <- NULL
 
   if (!is.numeric(x)) {
@@ -28,8 +22,10 @@ crosscut_slice <- function(x) {
       msg = paste0("sig must either be numeric or a list or ",
                    "data.frame containing a column named 'sig'"))
     sig <- x$sig
+    sig.n <- x
   } else {
     sig <- x
+    sig.n <- x
   }
 
   assertthat::assert_that(is.numeric(sig))
@@ -55,50 +51,59 @@ crosscut_slice <- function(x) {
   }
 
   sig_idx <- data_frame(
-    map_chunk = 0:length(idx),
+    map_cycle = 0:length(idx),
     map_start = c(1, idx),
     map_end = c(idx - 1, length(sig)),
-    map_type = c("start", rep(cycle, length(idx) - 1), "end")
-  )
+    map_type = c("start", rep(cycle, length(idx) - 1), "end"),
+    map_chunk = ceiling(map_cycle/ncycle)
+  ) %>%
+    group_by(map_chunk) %>%
+    summarize(map_start = min(map_start),
+              map_end = max(map_end),
+              map_type = ifelse(n_distinct(map_type) == 1, map_type[1],
+                                ifelse("end" %in% map_type, "end", "start")))
 
-  chunks <- purrr::pmap(sig_idx,
-                        function(map_chunk, map_start, map_end, map_type) {
-    z <- if (is.data.frame(x)) {
-      x[map_start:map_end,]
-    } else {
-      data_frame(
-        x = map_start:map_end,
-        sig = sig[map_start:map_end]
-      )
-    }
-    z$.type <- map_type
-
-    if (map_type == "end") {
-      # z$x <- -z$x + max(z$x)
-      z$sig <- rev(z$sig)
-    }
-
-    z <- z %>%
-      mutate(
-        x = x - min(x, na.rm = T),
-        sig = sig - med_val,
-        .chunk = map_chunk
-      )
-
-    # ensure chunks end on a negative note
-    last <- max(which(!is.na(z$sig)))
-    sgn <- z$sig[(last - 1):last] %>% sign() %>% mean(na.rm = T) %>% sign()
-    if ((map_type == "start" | map_type == "end" | map_type == "boundary") &
-        sgn > 0) {
-      z$sig <- -z$sig
-    }
-
-    z %>%
-      mutate(sig = ifelse(.type == "np", -sig, sig),
-             .type = ifelse(.type == "np", "pn", .type),
-             .type = ifelse(.type == "pn", "pn", "boundary")) %>%
-      mutate(x = ifelse(.type == 'boundary', x - max(x, na.rm = T), x))
-  })
+  chunks <- purrr::pmap(sig_idx, chunk_slice, sig = sig.n)
 
   chunks
+}
+
+chunk_slice <- function(sig, map_chunk, map_start, map_end, map_type) {
+  z <- if (is.data.frame(sig)) {
+    sig[map_start:map_end,]
+  } else {
+    data_frame(
+      x = map_start:map_end,
+      sig = sig[map_start:map_end]
+    )
+  }
+  z$.type <- map_type
+
+  med_val <- stats::median(z$sig, na.rm = T)
+
+  if (map_type == "end") {
+    # z$x <- -z$x + max(z$x)
+    z$sig <- rev(z$sig)
+  }
+
+  z <- z %>%
+    mutate(
+      x = x - min(x, na.rm = T),
+      sig = sig - med_val,
+      .chunk = map_chunk
+    )
+
+  # ensure chunks end on a negative note
+  last <- max(which(!is.na(z$sig)))
+  sgn <- z$sig[(last - 1):last] %>% sign() %>% mean(na.rm = T) %>% sign()
+  if ((map_type == "start" | map_type == "end" | map_type == "boundary") &
+      sgn > 0) {
+    z$sig <- -z$sig
+  }
+
+  z %>%
+    mutate(sig = ifelse(.type == "np", -sig, sig),
+           .type = ifelse(.type == "np", "pn", .type),
+           .type = ifelse(.type == "pn", "pn", "boundary")) %>%
+    mutate(x = ifelse(.type == 'boundary', x - max(x, na.rm = T), x))
 }
